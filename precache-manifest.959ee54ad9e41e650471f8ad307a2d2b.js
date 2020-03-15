2f2a98ca830fc39875684dbe1e0434f59d4aef7b
Sneha S S <snehasridhar115@gmail.com>
2020-03-15 15:40:17 +0530

    Updates

---
+++ precache-manifest.959ee54ad9e41e650471f8ad307a2d2b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c6fe6d429a4d34eeae73750dac8556a",
+    "url": "/ODML-Management/ODML-Management-Front-end/index.html"
+  },
+  {
+    "revision": "bdf970bdf36d3eeefead",
+    "url": "/ODML-Management/ODML-Management-Front-end/static/css/main.50530de6.chunk.css"
+  },
+  {
+    "revision": "9e3bd45cd1ced6094d02",
+    "url": "/ODML-Management/ODML-Management-Front-end/static/js/2.8b0174f6.chunk.js"
+  },
+  {
+    "revision": "bdf970bdf36d3eeefead",
+    "url": "/ODML-Management/ODML-Management-Front-end/static/js/main.490027c3.chunk.js"
+  },
+  {
+    "revision": "c356bb3f0d5171f77c68",
+    "url": "/ODML-Management/ODML-Management-Front-end/static/js/runtime~main.41bfe5b5.js"
+  }
+]);
